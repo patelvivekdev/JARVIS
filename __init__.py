eca44cbcc1c22bf38a47dedaddbc2afe5d13d1c6
vivek patel <vivek.170410107085@gmail.com>
2021-02-24 18:09:24 +0530

    Add Static Wake Word

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 # Import Tools
 import os
+import sys
+import configparser
+import random
+import speech_recognition as sr
 
 # Import Custom module
 from JARVIS.modules.face_identification import FaceRecognition
@@ -7,11 +11,83 @@ from JARVIS.modules.weather import Weather
 from JARVIS.modules.date import Date
 from JARVIS.modules.time import Time
 from JARVIS.modules.setup import Setup
+from JARVIS.modules.wake_word import WakeWord, DefaultFileNotFound
 
 
 class Jarvis:
     def _init__(self):
         pass
+
+    def hot_word_detect(self, lang='en'):
+        """
+        Hot word (wake word / background listen) detection
+        :param lang: str
+            default 'en'
+        :return: Bool, str
+            status, command
+        """
+        try:
+            status, command = WakeWord(lang=lang)
+            print(status, command)
+        except DefaultFileNotFound as e:
+            print(
+                "Unable to locate configuraton file './JARVIS/config/config.ini'. Creating NOW...")
+            self.setup()
+        except Exception as e:
+            status = command = False
+        return status, command
+
+    def mic_input(self, lang='en'):
+        """
+        Fetch input from mic
+        :param lang: str
+            default 'en'
+        :return: str/Bool
+            user's voice input as text if true/ false if fail
+        """
+        config = configparser.ConfigParser()
+        config.read('.JARVIS/config/config.ini')
+        user_name = config['default']['user_name']
+
+        try:
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                greeting = random.choice(["Hello", "Hi"])
+                msg = greeting + ' ' + user_name + ',' + ' How may I help you?'
+                print(msg)
+                self.text2speech(msg)
+                r.pause_threshold = 1
+                r.adjust_for_ambient_noise(source, duration=1)
+                audio = r.listen(source)
+            try:
+                command = r.recognize_google(audio, language=lang).lower()
+                print('You said: ' + command + '\n')
+            except sr.UnknownValueError:
+                print('....')
+                command = self.mic_input()
+            return command
+        except Exception as e:
+            print(e)
+            return False
+
+    def text2speech(self, text, lang='en'):
+        """
+        Convert any text to speech
+        :param text: str
+            text (String)
+        :param lang: str
+            default 'en'
+        :return: Bool
+            True / False (Play sound if True otherwise write exception to log and return False)
+        """
+        try:
+            pass
+            return True
+        except Exception as e:
+            mytext = "Sorry I couldn't understand, or not implemented to handle this input"
+            print(mytext)
+            print(e)
+            return False
 
     def tell_me_date(self):
         """
@@ -47,6 +123,18 @@ class Jarvis:
             print(e)
             res = False
         return res
+
+    def shutdown(self):
+        """
+        Shutdown the Jarvis API, exit from program
+        :return: None/bool
+            if no error then exit from program, False if Fail
+        """
+        try:
+            pass
+        except Exception as e:
+            print(e)
+            return False
 
     def setup(self):
         """
